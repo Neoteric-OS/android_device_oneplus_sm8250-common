9f275fba4ecb976221b167c1f6ba96249881145b
QKIvan <ivan33@163.com>
2025-02-12 21:05:50 +0800

    sm8250-common: Build libprotobuf full/lite 3.9.1
    Change-Id: I4ffdcc502d1bc72d563f88944deebad52484429f

--- common.mk
+++ common.mk
@@ -175,6 +175,11 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.2.vendor
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
+
 # QTI
 TARGET_BOARD_PLATFORM := kona
 
